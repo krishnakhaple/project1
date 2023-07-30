273e1da3f5cad395dfe1fb7bf482dfa42e5d33fd
Kkhap <Kkhap@jayshree1>
2023-07-30 16:17:50 +0530

    only one programme commiting

---
+++ project1/src/package1/Test.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+
+	}
+
+}
